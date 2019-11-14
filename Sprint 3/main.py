e3261f106fd7c9d7c35f342fb965ec3e262baf0b
ManonMic <manon.michelet5@gmail.com>
2019-11-14 11:34:46 -0500

    Add login window, refactoring

--- Sprint 3/main.py
+++ Sprint 3/main.py
@@ -4,6 +4,87 @@ import tkinter as tk
 def interface_gui():
     window = tk.Tk()
     window.title("Software softphone")
+
+    class LoginDialog(tk.Toplevel):
+        def __init__(self, parent):
+            super().__init__(parent)
+            self.title("Fenêtre Modale")
+            # ---------------------------------------
+            tk.Label(self, text="Login : ").pack()
+            self.entry_login = tk.Entry(self)
+            self.entry_login.insert(0, "defaultlogin")
+            self.entry_login.pack()
+            # ---------------------------------------
+            tk.Label(self, text="Clé : ").pack()
+            self.entry_pass = tk.Entry(self, show='*')
+            self.entry_pass.insert(0, "defaultpass")
+            self.entry_pass.pack()
+            # ---------------------------------------
+            tk.Button(self, text="Connexion", command=self.connect).pack()
+
+        def connect(self):
+            login = self.entry_login.get().strip()
+            key = self.entry_pass.get().strip()
+
+            self.destroy()
+
+    def connect():
+        """ Ouvre une fenêtre modale """
+        result = LoginDialog(window)
+        result.transient(window)
+        result.grab_set()
+        window.wait_window(result)
+
+    class Fenetre(tk.Frame):
+        def __init__(self, parent):
+            super().__init__(parent)
+            self.master.title("Fenêtre principale")
+            self.grid(row=1, column=1)
+            bouton_new = tk.Button(self, width=10, height=1, text="Connexion", command=connect)
+            bouton_new.grid(row=1, column=2)
+            # Affichage des chiffres
+            chiffre_1 = tk.Button(window, text="1", padx=20, command=lambda: add_char("1"))
+            chiffre_1.grid(row=2, column=1)
+            chiffre_2 = tk.Button(window, text="2", padx=20, command=lambda: add_char("2"))
+            chiffre_2.grid(row=2, column=2)
+            chiffre_3 = tk.Button(window, text="3", padx=20, command=lambda: add_char("3"))
+            chiffre_3.grid(row=2, column=3)
+            chiffre_4 = tk.Button(window, text="4", padx=20, command=lambda: add_char("4"))
+            chiffre_4.grid(row=3, column=1)
+            chiffre_5 = tk.Button(window, text="5", padx=20, command=lambda: add_char("5"))
+            chiffre_5.grid(row=3, column=2)
+            chiffre_6 = tk.Button(window, text="6", padx=20, command=lambda: add_char("6"))
+            chiffre_6.grid(row=3, column=3)
+            chiffre_7 = tk.Button(window, text="7", padx=20, command=lambda: add_char("7"))
+            chiffre_7.grid(row=4, column=1)
+            chiffre_8 = tk.Button(window, text="8", padx=20, command=lambda: add_char("8"))
+            chiffre_8.grid(row=4, column=2)
+            chiffre_9 = tk.Button(window, text="9", padx=20, command=lambda: add_char("9"))
+            chiffre_9.grid(row=4, column=3)
+            chiffre_0 = tk.Button(window, text="0", padx=20, command=lambda: add_char("0"))
+            chiffre_0.grid(row=5, column=2)
+            etoile = tk.Button(window, text="*", padx=20, command=lambda: add_char("*"))
+            etoile.grid(row=5, column=1)
+            diese = tk.Button(window, text="#", padx=20, command=lambda: add_char("#"))
+            diese.grid(row=5, column=3)
+            supp_char = tk.Button(window, text="<-", padx=20, command=delete_char)
+            supp_char.grid(row=6, column=1)
+
+            # Bouton pour appeler
+            appeler = tk.Button(window, text="Appeler", padx=50)
+            appeler.grid(row=6, column=2)
+
+            # Actions
+            voir_contact = tk.Button(window, text="Voir les contacts", command=display_contact)
+            voir_contact.grid(row=2, column=4)
+
+            boite_vocale = tk.Button(window, text="Ecouter messages")
+            boite_vocale.grid(row=3, column=4)
+
+            historique = tk.Button(window, text="Voir historique appels")
+            historique.grid(row=4, column=4)
+            window.mainloop()
+
     liste_affichage_numero = []
     liste_nom_contact = []
     liste_numero_contact = []
@@ -90,48 +171,7 @@ def interface_gui():
             supprimer_contact_bouton = tk.Button(window_contact, text="Supprimer", command=lambda: supprimer(contact))
             supprimer_contact_bouton.grid(row=contact, column=4)
 
-    # Affichage des chiffres
-    chiffre_1 = tk.Button(window, text="1", padx=20, command=lambda: add_char("1"))
-    chiffre_1.grid(row=2, column=1)
-    chiffre_2 = tk.Button(window, text="2", padx=20, command=lambda: add_char("2"))
-    chiffre_2.grid(row=2, column=2)
-    chiffre_3 = tk.Button(window, text="3", padx=20, command=lambda: add_char("3"))
-    chiffre_3.grid(row=2, column=3)
-    chiffre_4 = tk.Button(window, text="4", padx=20, command=lambda: add_char("4"))
-    chiffre_4.grid(row=3, column=1)
-    chiffre_5 = tk.Button(window, text="5", padx=20, command=lambda: add_char("5"))
-    chiffre_5.grid(row=3, column=2)
-    chiffre_6 = tk.Button(window, text="6", padx=20, command=lambda: add_char("6"))
-    chiffre_6.grid(row=3, column=3)
-    chiffre_7 = tk.Button(window, text="7", padx=20, command=lambda: add_char("7"))
-    chiffre_7.grid(row=4, column=1)
-    chiffre_8 = tk.Button(window, text="8", padx=20, command=lambda: add_char("8"))
-    chiffre_8.grid(row=4, column=2)
-    chiffre_9 = tk.Button(window, text="9", padx=20, command=lambda: add_char("9"))
-    chiffre_9.grid(row=4, column=3)
-    chiffre_0 = tk.Button(window, text="0", padx=20, command=lambda: add_char("0"))
-    chiffre_0.grid(row=5, column=2)
-    etoile = tk.Button(window, text="*", padx=20, command=lambda: add_char("*"))
-    etoile.grid(row=5, column=1)
-    diese = tk.Button(window, text="#", padx=20, command=lambda: add_char("#"))
-    diese.grid(row=5, column=3)
-    supp_char = tk.Button(window, text="<-", padx=20, command=delete_char)
-    supp_char.grid(row=6, column=1)
-
-    # Bouton pour appeler
-    appeler = tk.Button(window, text="Appeler", padx=50)
-    appeler.grid(row=6, column=2)
-
-    # Actions
-    voir_contact = tk.Button(window, text="Voir les contacts", command=display_contact)
-    voir_contact.grid(row=2, column=4)
-
-    boite_vocale = tk.Button(window, text="Ecouter messages")
-    boite_vocale.grid(row=3, column=4)
-
-    historique = tk.Button(window, text="Voir historique appels")
-    historique.grid(row=4, column=4)
-    window.mainloop()
+    window = Fenetre(window)
 
 
 if __name__ == '__main__':
